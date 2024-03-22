a62b09eec434158af7b4d2e865164d81148a12f1
Pranav K <pranavkcse@gmail.com>
2024-03-22 18:04:16 +0530

    fixed orderSchema

--- src/model/orderSchema.js
+++ src/model/orderSchema.js
@@ -1,30 +1,55 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
+const ObjectId = Schema.Types.ObjectId;
 
-const order_schema = new Schema(
+const orderSchema = new Schema(
   {
     customer_id: {
       type: ObjectId,
-      ref: 'User',
-      require: true,
+      ref: "User",
+      required: true,
     },
     items: [
       {
         product_id: {
           type: ObjectId,
-          ref: 'Product',
+          ref: "Product",
+          required: true,
+        },
+        variant: {
+          type: ObjectId,
+          ref: "Product.variants", // Corrected ref path
+          required: true,
+        },
+        color: {
+          type: ObjectId,
+          ref: "Color",
+        },
+        size: {
+          type: ObjectId,
+          ref: "Size",
           required: true,
         },
         quantity: {
           type: Number,
           required: true,
+          min: [1, `Quantity Can't be less than 1`],
         },
         price: {
           type: Number,
           required: true,
         },
+        itemTotal: {
+          type: Number,
+        },
+        orderID: {
+          type: String,
+          unique: true,
+        },
         status: {
+          type: String,
+        },
+        returnReason: {
           type: String,
         },
         shipped_on: {
@@ -41,29 +66,39 @@ const order_schema = new Schema(
         },
       },
     ],
-    address: {
-      type: mongoose.Schema.Types.Mixed,
+    shippingAddress: {
+      type: ObjectId,
+      ref: "Address",
       required: true,
     },
-    payment_method: {
+    paymentMethod: {
       type: String,
       required: true,
     },
-    total_amount: {
+    totalPrice: {
       type: Number,
       required: true,
+      min: 0,
     },
     coupon: {
-      coupon_id: {
-        type: ObjectId,
-        ref: "Coupon",
-      },
-      discount: {
-        type: Number,
-      },
-      code: {
-        type: String,
-      },
+      type: ObjectId,
+      ref: "Coupon",
+    },
+    couponDiscount: {
+      type: Number,
+      default: 0,
+    },
+    payable: {
+      type: Number,
+    },
+    categoryDiscount: {
+      type: Number,
+      default: 0,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["paid", "pending", "cod", "failed", "refunded", "cancelled"],
+      // required: true,
     },
     status: {
       type: String,
@@ -75,4 +110,5 @@ const order_schema = new Schema(
   }
 );
 
-module.exports = mongoose.model("Order", order_schema);
+
+module.exports = mongoose.model("Order", orderSchema);
