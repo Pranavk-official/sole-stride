3d2c27bfa68231deb8f3a3c1ecb181344e8cec0a
Pranav K <pranavkcse@gmail.com>
2024-03-22 18:02:06 +0530

    added function to get category details and send to frontend

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -16,6 +16,25 @@ module.exports = {
       layout,
     });
   },
+  getCategoryDetails: async (req, res) => {
+    const categoryId = req.params.id
+    try {
+      const category = await Category.findOne({_id: categoryId});
+      
+      if (!category) {
+        return res.status(404).json({ error: "Category not found" });
+      }
+
+      return res.status(200).json({
+        success: true,
+        category
+      });
+    } catch (error) {
+      console.log("Error:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    
+  },
   getAddCategory: async (req, res) => {
     res.render("admin/categories/addCategory", {
       layout,
