c6e8af794e3f6cdad076931546786541ecc10602
Pranav K <pranavkcse@gmail.com>
2024-04-16 13:23:44 +0530

    flash msg test

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -16,11 +16,11 @@ const {
   isAdminLoggedOut,
 } = require("../middlewares/authMiddleware");
 
-router.use((req,res,next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next()
-})
+// router.use((req,res,next) => {
+//   res.locals.success = req.flash("success");
+//   res.locals.error = req.flash("error");
+//   next()
+// })
 
 
 router
